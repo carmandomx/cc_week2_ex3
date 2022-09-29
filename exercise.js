0dceb3b4cb1a119bf485850fbf31a81760d71c39
YumilRuedaFlores-Ksquare <Yumil.Flores@itksquare.edu.mx>
2022-09-29 20:00:54 +0200

    DONE

--- exercise.js
+++ exercise.js
@@ -10,6 +10,14 @@
 const spinal = (str) => {
     /* Only make changes below this comment */
     
+    //regular expresion to put an space before every mayuscule
+    const regex =  /(?!^)([A-Z])/g;
+    let result = str.replace(regex, ' $1');
+    //regular expresion for change the space and letters+Mayuscule by a bar
+    const regex_2= /[\s]+(?=[a-zA-Z])/g;
+    result = result.replace(regex_2, '-').toLowerCase();
+
+    return result;
     
     /* Only make changes below this comment */
 }
