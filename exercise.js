352e359ec26ed82afbec0e1648ee43618aabf256
MarianaLopez-Ksquare <Mariana.Lopez@itksquare.edu.mx>
2022-09-28 21:12:45 -0500

    First commit

--- exercise.js
+++ exercise.js
@@ -9,13 +9,17 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
     /* Only make changes below this comment */
-    
-    
+
+// The str_replace() function replaces some characters with some other characters in a string. 
+ // Replace low-upper case to low-space-uppercase
+   
+        let spinal = str.replace(/(?!^)([A-Z])/g, ' $1')
+
+// Replace space and underscore with -
+                      .replace(/[_\s]+(?=[a-zA-Z])/g, '-').toLowerCase();
+        return spinal 
     /* Only make changes below this comment */
 }
-
-
-
 
 
 /** DO NOT CHANGE THE LINE BELOW **/
