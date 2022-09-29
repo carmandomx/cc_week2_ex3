800adfd054e3b735605e721563f3af08558a2eb2
Junior Cabrera <junior.cabrera@itksquare.edu.mx>
2022-09-29 14:37:05 -0400

    Ex 3 - Done

--- exercise.js
+++ exercise.js
@@ -9,8 +9,13 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
     /* Only make changes below this comment */
-    
-    
+
+    str = str.replace(/([A-Z])/g,' $1'); //Regex to find capital letters and add a space in front of them.
+    str = str.replace(/\s{1,}/g,"-"); //Regex to convert spaces to dash (-).
+    str = str.replace(/^\-|[\-]$/g,'');//I eliminate the hyphen at the end and at the beginning in case there was a capital letter in them as it is validated at the beginning.
+    str = str.toLowerCase(); //I convert to lowercase.
+
+    return str;
     /* Only make changes below this comment */
 }
 
