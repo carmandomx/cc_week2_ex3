cd62d1bbf676c2e1f35af6ad2a85fb8ba014b49c
Axcel Espinal <axcel.espinal@itksquare.edu.mx>
2022-09-29 10:12:29 -0400

    Solved the exercise

--- exercise.js
+++ exercise.js
@@ -9,18 +9,17 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
     /* Only make changes below this comment */
-    str = str.split("");
-    for(i=0; i<str.length; i++) {
-        if (str[i] === str[i].toUpperCase()) {
-            
-        }
-        else if(str[i]===" ") {
+    str = str.replace(/([a-z])([A-Z])/g, '$1 $2'); //use the replace method with the regular expression of a lowercase next to an uppercase and put a space between them
+    str = str.replace(/([A-Z])([A-Z])/g, '$1 $2'); //use the replace method with the regular expression of an uppercase next to another and put a space between them
+    str = str.split(""); //split the string into an array
+    for(let i=0; i<str.length; i++) { //for loop to iterate through the array
+        if(str[i] === " ") { //if the character is a space, replace it with a -
             str[i] = "-";
         }
-        str[i] = str[i].toLowerCase();
+        str[i] = str[i].toLowerCase(); //set the uppercases lo lowercases
     }
-    str = str.join("");
-    return str;
+    str = str.join(""); //turn the array back to a string
+    return str; //return the string
     /* Only make changes below this comment */
 }
 
