fc207b8373bab4a0c75324275b0155642cbac2b4
Ivanna Nouel <Ivanna.Nouel@itksquare.edu.mx>
2022-09-29 13:57:33 -0500

    Done

--- exercise.js
+++ exercise.js
@@ -9,9 +9,21 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
     /* Only make changes below this comment */
-    
-    
+
+    //This regular expression is used to put an space in front of every UpperCase letter 
+    const spinal = str.replace(/(?!^)([A-Z])/g, ' $1');
+
+    //This is to add a dash in the spaces and in front of the uppercase letters
+    const spinal2 = spinal.replace(/[_\s]+(?=[a-zA-Z])/g, '-');
+
+    //this changes all letters to lower case
+    return spinal2.toLowerCase();
+
+
     /* Only make changes below this comment */
+
+
+
 }
 
 
