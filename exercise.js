6349a49db83189d6cab9fd8c1be0ed0580de139f
Axcel Espinal <axcel.espinal@itksquare.edu.mx>
2022-09-29 08:54:49 -0400

    Initial commit

--- exercise.js
+++ exercise.js
@@ -9,14 +9,20 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
     /* Only make changes below this comment */
-    
-    
+    str = str.split("");
+    for(i=0; i<str.length; i++) {
+        if (str[i] === str[i].toUpperCase()) {
+            
+        }
+        else if(str[i]===" ") {
+            str[i] = "-";
+        }
+        str[i] = str[i].toLowerCase();
+    }
+    str = str.join("");
+    return str;
     /* Only make changes below this comment */
 }
-
-
-
-
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.spinal = spinal;
