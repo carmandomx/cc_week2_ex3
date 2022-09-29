8ecdd1d579886885267599561ec72dac42fb54d5
Jonathan castillo <jonathan.castillo.itksquare.edu.mx>
2022-09-29 15:41:35 -0500

    Upload Spinal-case file solved and tested

--- __tests__/suite.test.js
+++ __tests__/suite.test.js
@@ -1,7 +1,7 @@
 const { spinal } = require('../exercise');
 
-test('Testing the spinal exercise', () => { 
+test('Testing the spinal exercise', () => {
     expect(spinal('this is a test')).toBeDefined();
     expect(spinal('this is a Test')).toBe('this-is-a-test')
     expect(spinal('ThisIsATest')).toBe('this-is-a-test')
- })
+})
