c24f0dd74ad3452cebb7cf55d6650e50fd6a3e3d
Eduardo De Jesus <eduardo.dejesus@itksquare.edu.mx>
2022-09-30 09:53:47 -0400

    finished the exercise with some issues

--- exercise.js
+++ exercise.js
@@ -9,7 +9,15 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
     /* Only make changes below this comment */
-    
+
+     // Create a variable for the white space and underscores.
+        var regexString = /[_\s]+(?=[a-zA-z])|/g;
+
+    // Replacing lower and upper case to the same but with a space
+        str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+
+    // Replace space and underscore with -
+        return str.replace(regexString, "-").toLowerCase();
     
     /* Only make changes below this comment */
 }
