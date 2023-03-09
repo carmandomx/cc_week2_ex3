50f54bda9186d4e4053a5762275f839ef57a0b48
Alexander Iván Suárez Vivas <alexander.suarez@itksquare.edu.mx>
2023-03-09 13:15:02 -0600

    Ejercicio Resuelto

--- exercise.js
+++ exercise.js
@@ -5,18 +5,15 @@
 
 */
 
-
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
-    /* Only make changes below this comment */
-    
-    
-    /* Only make changes below this comment */
-}
-
-
-
-
+  /* Only make changes below this comment */
+  let camelCase = str.replace(/([a-z])([A-Z])/g, "$1 $2"); //Agrega un espacio entre letras minúsculas y mayúsculas.
+  let dobleUpper = camelCase.replace(/([A-Z])([A-Z])/g, "$1 $2"); //Agrega un espacio entre dos letras mayúsculas
+  let spinal = dobleUpper.replace(/\s|_/g, "-"); //Reemplaza todos los espacios y guiones bajos por guiones.
+  return spinal.toLowerCase();
+  /* Only make changes below this comment */
+};
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.spinal = spinal;
