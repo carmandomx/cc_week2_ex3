ca1d5456a352d4c9e828115b74e2212ee7d127c3
Rita Torres <taraumara96@gmail.com>
2023-03-09 17:08:28 -0600

    SpinalNuevo

--- exercise.js
+++ exercise.js
@@ -5,17 +5,16 @@
 
 */
 
-
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
-    /* Only make changes below this comment */
-    
-    
-    /* Only make changes below this comment */
+  /* Only make changes below this comment */
+  let nuevo= str.replace(/(?!^)([A-Z])/g,' $1').replace(/[_\s]+(?=[a-zA-Z])/g,'-').toLowerCase();
+return nuevo;
+
+
 }
 
-
-
+  
 
 
 /** DO NOT CHANGE THE LINE BELOW **/
