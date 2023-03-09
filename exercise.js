62b834522c0bfcf0c4a3ecfdb9354aa78604243e
Rita Torres <taraumara96@gmail.com>
2023-03-09 17:20:44 -0600

    SpinalCase

--- exercise.js
+++ exercise.js
@@ -8,14 +8,12 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
   /* Only make changes below this comment */
-  let nuevo= str.replace(/(?!^)([A-Z])/g,' $1').replace(/[_\s]+(?=[a-zA-Z])/g,'-').toLowerCase();
-return nuevo;
-
-
-}
-
-  
-
+  let nuevo = str
+    .replace(/(?!^)([A-Z])/g, " $1")
+    .replace(/[_\s]+(?=[a-zA-Z])/g, "-")
+    .toLowerCase();
+  return nuevo;
+};
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.spinal = spinal;
