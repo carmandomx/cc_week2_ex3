32ebd82fe35fb6e7fcbc57da97e1b26bf81acb14
Alan Villanueva <alan.villanueva@itksquare.edu.mx>
2023-03-09 10:54:42 -0600

    solved

--- exercise.js
+++ exercise.js
@@ -9,14 +9,13 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
     /* Only make changes below this comment */
+
+    const regex= /(\s(\w))|((?<=\w)[A-Z])/g //two cases: 1 if a character next to space (match with character). 2 character next to uppper case letter (match with upper case)
+    dashed_str=str.replace(regex,'-$2$3') //replace matches with dash next to match
+    return dashed_str.toLowerCase()//change to lower case
     
     
     /* Only make changes below this comment */
 }
-
-
-
-
-
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.spinal = spinal;
