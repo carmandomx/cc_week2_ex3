3967c8a017b6053d08c38e561e15bc435c83a24c
Andrei Santos <Andrei.Santos@itksquare.edu.mx>
2023-03-09 16:24:29 -0600

    cc_week2_ex3 AndreiSantos

--- exercise.js
+++ exercise.js
@@ -9,8 +9,10 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
     /* Only make changes below this comment */
-    
-    
+    return (str.split('')).map( (element, index ) => 
+        index > 0 ? element === ' ' ? str[index+1].match(/[A-Z]/) ?
+        '' : '-' : element.replace(/[A-Z]/, '-' + element) : element )
+        .join('').toLowerCase();
     /* Only make changes below this comment */
 }
 
