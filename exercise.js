7b107cad11d68db1b3d70d03d881628a292f7147
BraulioEstrada-Ksquare <Braulio.Estrada@itksquare.edu.mx>
2023-03-09 15:39:04 -0600

    ejercicio resuelto

--- exercise.js
+++ exercise.js
@@ -9,8 +9,10 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
     /* Only make changes below this comment */
-    
-    
+    let spin = str.replace(/(?!^)([A-Z])/g, ' $1'); //Evualte first condition, before any Uppercase that is not the one in the beggining of the string, we add a space
+    let spin2 = spin.replace(/[\s]+([a-zA-Z])/g, '-$1');//We look for a space that is followed by any letter and we switch it for a -
+    let result = spin2.toLowerCase(); // we lower case all the remaining characters
+    return result; 
     /* Only make changes below this comment */
 }
 
