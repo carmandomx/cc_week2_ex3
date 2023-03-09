3cf3d1295da904eb9dec81d42345c2256aea3a16
SergioEspadas-Ksquare <125910030+SergioEspadas-Ksquare@users.noreply.github.com>
2023-03-09 10:17:58 -0600

    Add files via upload

--- exercise.js
+++ exercise.js
@@ -9,10 +9,16 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
     /* Only make changes below this comment */
-    
-    
-    /* Only make changes below this comment */
+let re = /\s|_|(?=[A-Z])/g; //find all the spaces, low dashed and, uppercase lettters  
+let re2 = /[.,/¿?*]/g //find some non-alphanumerical characters
+let newStr = str.split(re).join('-').replace(re2,"").toLowerCase(); 
+//split all the words and joins it with a "-" then if there is a non-alphanumerical character replace it with ""
+
+    return newStr
 }
+    /* Only make changes below this comment */
+
+console.log(spinal("Hello_world, my is Sergio? 2*5"))
 
 
 
