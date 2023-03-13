4efaf6f21da2a5dce5c559cfe69c6fa44fbd1665
Rita Torres <taraumara96@gmail.com>
2023-03-13 17:18:42 -0600

    Add comments

--- exercise.js
+++ exercise.js
@@ -9,8 +9,14 @@
 const spinal = (str) => {
   /* Only make changes below this comment */
   let nuevo = str
+  ///(?!^)([A-Z])/g is used to find any capital letter that appears in the middle of the string 
+  //$1 add a space before the marched capital letter
+  //g is a global flag that indicates to be applied to all occurrences of the pattern in the input string
     .replace(/(?!^)([A-Z])/g, " $1")
+    ///[_\s] matches any sequence of one or more uderscores or whitespace characters
+    //(?=[a-zA-Z])/g matches any position in the string that is immediately followed by a letter
     .replace(/[_\s]+(?=[a-zA-Z])/g, "-")
+    //convert all the string in a lowerCase 
     .toLowerCase();
   return nuevo;
 };
