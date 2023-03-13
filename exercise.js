f0bef1ba704dc6f820b27f9565334d7f1a61dbd0
Andrei Santos <Andrei.Santos@itksquare.edu.mx>
2023-03-13 17:35:29 -0600

    Commit for Comments AndreiSantos

--- exercise.js
+++ exercise.js
@@ -9,7 +9,17 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
     /* Only make changes below this comment */
+
+    // split str and star a map
     return (str.split('')).map( (element, index ) => 
+        // the process of map works in the next way:
+        // it only return element if index is not 0 or the element equals ' '.
+        // if the element equals ' ' and the next element match with regex for Uppercase
+        // it will return ''. If the next element is not on Uppercase it will return a
+        // '-'. In casem the actual element is not a ' ' it will replace the Uppercase
+        // with an '-' and the actual element.
+
+        // at the end, join the splited str again and convert str to Lowecase.
         index > 0 ? element === ' ' ? str[index+1].match(/[A-Z]/) ?
         '' : '-' : element.replace(/[A-Z]/, '-' + element) : element )
         .join('').toLowerCase();
